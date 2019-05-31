d4850163a06d905d5becd61e51d78de21de79dcd
YoonaKim25 <oliviakim0205@gmail.com>
2019-05-31 22:01:33 +0900

    database 수정

--- crudproject/settings.py
+++ crudproject/settings.py
@@ -129,5 +129,5 @@ STATIC_URL = '/static/'
 # settings.py
 # # Heroku : Update database configuration from $DATABASE_URL.
 import dj_database_url
-db_from_env = dj_databse_url.config(conn_max_age=500)
-DATABASE['default'].update(db_from_env)
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
