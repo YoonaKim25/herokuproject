abffd4edfcef7d190086394f3d31dc3ed9348653
YoonaKim25 <oliviakim0205@gmail.com>
2019-05-31 22:17:29 +0900

    수정2

--- crudproject/settings.py
+++ crudproject/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'd9ing3*l8gaki#1!s=z)_$-24coppg
 #DEBUG = True
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
